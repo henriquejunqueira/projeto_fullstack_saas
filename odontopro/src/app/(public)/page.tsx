c7bc0339f5a859a9bf020a1491c7a8232bcb8583
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-04-11 00:33:08 -0300

    Criando componente hero landing page

--- odontopro/src/app/(public)/page.tsx
+++ odontopro/src/app/(public)/page.tsx
@@ -1,9 +1,14 @@
 import { Header } from './_componentes/header';
+import { Hero } from './_componentes/hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <Header />
+
+      <div>
+        <Hero />
+      </div>
     </div>
   );
 }
